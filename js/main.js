5e3f96160a62a6fc8a008bedb1141e499fc4555f
nickvalin1 <nvalin@masonlive.gmu.edu>
2016-02-25 02:33:19 -0500

    Working Game

--- js/main.js
+++ js/main.js
@@ -1,52 +1,75 @@
 window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
     
     "use strict";
     
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+    game.load.image('spaceship', 'assets/spaceship.png');
+    game.load.image('space', 'assets/background.jpg');
+    game.load.image('asteroid', 'assets/asteroid.png');
     }
     
-    var bouncy;
-    
+    var player;
+    var background;
+    var asteroids;
+    var cursors;
+    var dead=false;
+    var score=0;
+    var scoreText;
+    var deathText;
     function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
+        background=game.add.sprite(0,0,'space');
+        background.scale.setTo(.5,.5);
         
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+        player=game.add.sprite(55,250,'spaceship');
+        game.physics.arcade.enable(player);
+        player.scale.setTo(.2,.2);
+        player.rotation=1.570795;
+        player.body.collideWorldBounds=true;
+        player.body.setSize(250,400,-50);
+       
+        scoreText=game.add.text(16,16,"Score: "+score, {fontSize: '28px', fill:'#ffffff', align: 'center'});
+        
+        asteroids=game.add.group();
+        asteroids.enableBody=true;
+        game.time.events.loop(Phaser.Timer.SECOND, makeAsteroid, this);
+        game.time.events.loop(Phaser.Timer.SECOND, increaseScore, this);
+        
+        cursors = game.input.keyboard.createCursorKeys();
+    }
+    
+    function makeAsteroid() {
+        var asteroid=asteroids.create(750,Math.random()*600, 'asteroid');
+        asteroid.scale.setTo(.2,.2);
+        asteroid.body.velocity.x=-200;
+    }
+    
+    function increaseScore() {
+        if (!dead) {
+            score++;
+            scoreText.text="Score: "+score;
+        } 
+    }
+    
+    function death() {
+        dead=true;
     }
     
     function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+        game.physics.arcade.collide(player, asteroids,death,null,this);
+        if (!dead) {
+            if (cursors.up.isDown) {
+                player.body.velocity.y=250;
+            }
+            else if (cursors.down.isDown) {
+                player.body.velocity.y=-250;
+            }
+        }
+        else {
+            deathText=game.add.text(350,200,"You Lose!", {fontSize: '128px', fill:'#ffffff', align: 'center'});
+        }
     }
 };
